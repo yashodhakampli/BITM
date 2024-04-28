deee54c2eaf6352e0d701c51d9d8abfe02c2ddad
yashodhakampli <167276574+yashodhakampli@users.noreply.github.com>
2024-04-28 14:35:45 +0530

    Add files via upload

---
+++ tuple1.py
@@ -0,0 +1,31 @@
+a3=[10,20,30,-10,"hello",True,20.55]
+print(a3)
+print(type(a3))
+#tuples
+a=(10,20,30,-10,"hello",200.55,20,False) # ordered
+print(a)
+print(type(a))
+# a[5]=300.55
+# print(a)
+
+'''a1=[10,20,30,-10,"hello",200.55,20]
+a1[5]=300.55
+print(a1)'''
+
+a2=(100,200,300,[150,250,350],400,500)
+print(a2[3][-2])
+
+
+a3=[1000,2000,3000,(1001,2001,3001),4000,5000]
+print(a3[3])
+
+b=(10,)
+print(type(b))
+c=(100,200,300,("hello","hai","take care"),(10.5,20.5,30.5))
+print(c)
+#c[1]=2000.5
+#print(c)
+print(a[3])
+print(c[4][1])
+print(c[4])
+print(a[-1])
